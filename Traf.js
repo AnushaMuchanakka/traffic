6ad54d69d18710e33baa30344d59a9276af486ce
AnushaMuchanakka <anushamucchanakka@gmail.com>
2025-07-21 21:46:40 +0530

    Add files via upload

---
+++ Traf.js
@@ -0,0 +1,19 @@
+
+function redbtn(){
+redbtns.style.backgroundColor="red";
+orangebtns.style.backgroundColor="grey";
+ greenbtns.style.backgroundColor="grey";
+ 
+}
+function orgbtn()
+{
+    orangebtns.style.backgroundColor="orange";
+    redbtns.style.backgroundColor="grey";
+    greenbtns.style.backgroundColor="grey";
+
+}
+function greebtn(){
+     redbtns.style.backgroundColor="grey";
+    orangebtns.style.backgroundColor="grey";
+    greenbtns.style.backgroundColor="green";
+}
